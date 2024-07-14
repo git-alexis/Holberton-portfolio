0cfd3bf058c8116600b56370004227494d0d0827
git-alexis <alex59.billemont@gmail.com>
2024-07-15 00:13:30 +0200

    add styles for form register and login and js for zoom and dezoom page

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,36 @@
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Fonction pour basculer la classe de l'image
+function toggleZoom(event) {
+    const element = event.target;
+    element.classList.toggle('img-zoomed');
+}
+
+// Ajout de l'écouteur d'événements à chaque image
+document.getElementById('image1').addEventListener('click', toggleZoom);
+document.getElementById('image2').addEventListener('click', toggleZoom);
+
+/*document.addEventListener('DOMContentLoaded', function(event) {
+    var thumbnail_1Element = document.getElementById("smart_thumbnail_1");
+    var thumbnail_2Element = document.getElementById("smart_thumbnail_2");
+    if (thumbnail_1Element) {
+        thumbnail_1Element.addEventListener("click", function() {
+            if (thumbnail_1Element.className == "") {
+                thumbnail_1Element.className = "small";
+            } else {
+                thumbnail_1Element.className = "";
+            }
+        });
+    }
+    if (thumbnail_2Element) {
+        thumbnail_2Element.addEventListener("click", function() {
+            if (thumbnail_2Element.className == "") {
+                thumbnail_2Element.className = "small";
+            } else {
+                thumbnail_2Element.className = "";
+            }
+        });
+    }
+});*/
