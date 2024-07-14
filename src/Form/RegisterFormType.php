7eac8f04c2d118cb4771c50b412b832d970b8c8e
git-alexis <alex59.billemont@gmail.com>
2024-07-15 00:09:21 +0200

    add add name and add surname method for the register form

--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -12,18 +12,34 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 class RegisterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('name', TextType::class, [
+                'label' => '* Firstname :',
+            ])
+            ->add('surname', TextType::class, [
+                'label' => '* Lastname :',
+            ])
             ->add('email', EmailType::class, [
-                'label' => 'Email',
+                'label' => '* Email :',
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => '* Roles :',
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                ],
+                'multiple' => true,
+                'expanded' => true,
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'first_options' => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options' => ['label' => '* Password :'],
+                'second_options' => ['label' => '* Repeat Password :'],
                 'invalid_message' => 'The password fields must match.',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
