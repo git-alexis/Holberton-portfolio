42ce3c89ff24096bb0cb8d7564c93f44e19530d4
git-alexis <alex59.billemont@gmail.com>
2024-07-19 14:19:08 +0200

    add compare method for tank api

--- src/Controller/TankController.php
+++ src/Controller/TankController.php
@@ -11,10 +11,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\SecurityBundle\Security;
+/*use App\Service\TankApiClient;*/
 
 #[Route('/tank')]
 class TankController extends AbstractController
 {
+	/*private $tankApiClient;
+
+    public function __construct(TankApiClient $tankApiClient)
+    {
+        $this->tankApiClient = $tankApiClient;
+    }*/
+
     #[Route('/', name: 'app_tank_index', methods: ['GET'])]
     public function index(TankRepository $tankRepository, Security $security): Response
     {
@@ -26,6 +34,20 @@ class TankController extends AbstractController
 			'user' => $user,
         ]);
     }
+
+	/*#[Route('/compare/{tankId1}/{tankId2}', name: 'app_tank_compare', methods: ['GET'])]
+    public function compare(int $tankId1, int $tankId2, Security $security): Response
+    {
+        $user = $security->getUser();
+        $tank1 = $this->tankApiClient->fetchTankDetails($tankId1);
+        $tank2 = $this->tankApiClient->fetchTankDetails($tankId2);
+
+        return $this->render('tank/compare.html.twig', [
+            'tank1' => $tank1,
+            'tank2' => $tank2,
+            'user' => $user,
+        ]);
+    }*/
 
     #[Route('/new', name: 'app_tank_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager, Security $security): Response
